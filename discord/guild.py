784c9c2e27c4eaa70b5937a4dc5fe78d96647b77
BeatButton <thebeatbutton@gmail.com>
2026-02-24 20:31:38 -0700

    rollup guild properties into guild dict

--- discord/guild.py
+++ discord/guild.py
@@ -575,6 +575,7 @@ class Guild(Hashable):
         return cls(state=state, data=data)  # type: ignore
 
     def _from_data(self, guild: GuildPayload) -> None:
+        guild = {**guild, **guild['properties']}  # type: ignore
         try:
             self._member_count = guild['member_count']  # pyright: ignore[reportTypedDictNotRequiredAccess]
         except KeyError:
